786f30946502e8db1393984fc66f6167e5af90c3
Zach <zach.johnson070@gmail.com>
2016-05-06 11:14:57 -0600

    defined all the actions and reducers

--- app/components/App.js
+++ app/components/App.js
@@ -6,10 +6,16 @@ import ReactDOM from 'react-dom';
 
 import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 
+import {createStore} from 'redux';
+
 import MainMenu from './MainMenu';
 import JoinGame from './JoinGame';
 import Frame from './Frame';
 import CreateGame from './CreateGame';
+
+import AppReducer from '../reducers/AppReducer.js';
+
+let store = createStore(AppReducer);
 
 var App = <Router history={browserHistory}>
 			<Route path="/" component={Frame} >
